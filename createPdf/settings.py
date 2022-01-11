2c52e27f4b4738c41a7a6740ef1eb0be7aed44f5
Damian <damianmudzengerere@gmail.com>
2022-01-11 17:07:38 +0200

    Changed sqllite db to Postgresql

--- createPdf/settings.py
+++ createPdf/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'createPdf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Damian',
+        'USER': 'postgres',
+        'PASSWORD': 'endurancedame',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
